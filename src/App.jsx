e7b31d9540ab7059363867fb7d2aacefb316e47d
GRIM-devRony <107187284+GRIM-devRony@users.noreply.github.com>
2022-09-30 16:05:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import "./app.css";
+const App = () => {
+  return (
+    <div id="app">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
